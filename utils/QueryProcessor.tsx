eb72d590826ca3d4256f8d5f6d5822f16ead814b
lsharma-21 <lsharma@andrew.cmu.edu>
2026-02-24 15:19:06 +0000

    updated name again

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "lsharma";
   }
 
   if (query.toLowerCase().includes("andrew")) {
